a4585be34847d33b738ad0dcbddcdeb7e95c984a
Domowe <marcin_nowak_hrx@o2.pl>
2023-03-01 21:55:44 +0100

    add rudimentary display of weather data

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //by location 'http://api.openweathermap.org/data/2.5/weather?q=Torun&APPID=762d23cb7577413f8fba8f728324cb17&units=metric '
 //by coordinates 'http://api.openweathermap.org/data/2.5/weather?q=Torun&APPID=762d23cb7577413f8fba8f728324cb17&units=metric'
+//onecall
 //Toruń
   //lon	18.5981
   //lat	53.0137
 //units metric default imperial add picker
   //use geocoding to get location https://openweathermap.org/api/geocoding-api
   //then using the longitude and latitude fetch the weather data
+
+  //Photo by Rafael Cerqueira: https://www.pexels.com/photo/blue-and-white-sky-with-stars-4737484/
 let currentWeather = null//not used yet
 let units = "metric"
 const AppId = "762d23cb7577413f8fba8f728324cb17"
@@ -14,6 +17,17 @@ let currentLocation = {
   lat: 53.0137
 }
 
+const currentTemperature = document.getElementById("current-temperature")
+const weather = document.getElementById("weather")
+const humidity = document.getElementById("humidity")
+const feelsLike = document.getElementById("feels-like")
+const windSpeed = document.getElementById("wind-speed")
+
+
+function displayWeatherParameter (variable, value) {
+  variable.innerText = value
+  
+}
 
 
 async function getWeather(){
@@ -27,7 +41,16 @@ async function getWeather(){
 }
 
 function displayWeather(){
-  getWeather().then(data => console.log(data))
+  getWeather().then((data) => {
+    console.log(data)
+    currentWeather = data
+    
+    displayWeatherParameter(currentTemperature, currentWeather.main.temp)
+    displayWeatherParameter(weather, currentWeather.weather[0].main)
+    displayWeatherParameter(humidity, currentWeather.main.humidity)
+    displayWeatherParameter(feelsLike, currentWeather.main.feels_like)
+    displayWeatherParameter(windSpeed, currentWeather.wind.speed)
+  })
 }
 
 displayWeather()
